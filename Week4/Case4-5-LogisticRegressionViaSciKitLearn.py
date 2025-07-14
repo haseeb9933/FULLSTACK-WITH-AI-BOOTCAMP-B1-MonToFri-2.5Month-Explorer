7705f2390f40700acaa06938329405e53106a26e
haseeb9933 <contact.haseeb1@gmail.com>
2025-07-14 15:16:21 +0500

    Update Case4-5-LogisticRegressionViaSciKitLearn.py

--- Week4/Case4-5-LogisticRegressionViaSciKitLearn.py
+++ Week4/Case4-5-LogisticRegressionViaSciKitLearn.py
@@ -9,7 +9,7 @@ https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticR
 import pandas as pd
 col_names = ['pregnant', 'glucose', 'bp', 'skin', 'insulin', 'bmi', 'pedigree', 'age', 'label']
 # load dataset
-pima = pd.read_csv("week5/diabetes.csv", header=1, names=col_names)
+pima = pd.read_csv("/Users/haseebtariq/Documents/GitHub/FULLSTACK-WITH-AI-BOOTCAMP-B1-MonToFri-2.5Month-Explorer/Week4/diabetes.csv", header=1, names=col_names)
 
 """
 
